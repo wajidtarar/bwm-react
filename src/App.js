f3600fc3500f4943aefd298349bb95b19264fdd5
Wajid <wajid.tarar@gmail.com>
2018-09-18 15:06:54 +0500

    188 Redux exciting start
    188 Redux exciting start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect} from 'react-router-dom';
 
+import * as redux from 'redux';
+import { Provider } from 'react-redux';
+
 import { Header } from './shared/Header';
-import { RentalList } from './components/rental/RentalList';
+import  RentalList  from './components/rental/RentalList';
 import { RentalDetail } from './components/rental/RentalDetail';
 
 import './App.css';
@@ -10,18 +13,78 @@ import './App.css';
 class App extends Component {
 
   render() {
-    return (
-      <BrowserRouter>
-        <div className='App'>
-          <Header/>
-          <div className='container'>
-            <Route exact path = '/'  render = { () => { return <Redirect to='rentals' />} } />
-            <Route exact path = '/rentals' component={RentalList} />
-            <Route exact path = '/rentals/:id' component={RentalDetail} />
-          </div>
 
-        </div>
-      </BrowserRouter>
+    const store = redux.createStore( () => {
+      return {
+        rentals: [{
+          id: 1,
+          title: "Central Apartment",
+          city: "New York",
+          street: "Times Sqaure",
+          category: "apartment",
+          image: "http://via.placeholder.com/350x250",
+          bedrooms: 3,
+          description: "Very nice apartment",
+          dailyRate: 34,
+          shared: false,
+          createdAt: "24/12/2017"
+        },
+        {
+          id: 2,
+          title: "Central Apartment 2",
+          city: "San Francisco",
+          street: "Main street",
+          category: "condo",
+          image: "http://via.placeholder.com/350x250",
+          bedrooms: 2,
+          description: "Very nice apartment",
+          dailyRate: 12,
+          shared: true,
+          createdAt: "24/12/2017"
+        },
+        {
+          id: 3,
+          title: "Central Apartment 3",
+          city: "Bratislava",
+          street: "Hlavna",
+          category: "condo",
+          image: "http://via.placeholder.com/350x250",
+          bedrooms: 2,
+          description: "Very nice apartment",
+          dailyRate: 334,
+          shared: true,
+          createdAt: "24/12/2017"
+        },
+        {
+          id: 4,
+          title: "Central Apartment 4",
+          city: "Berlin",
+          street: "Haupt strasse",
+          category: "house",
+          image: "http://via.placeholder.com/350x250",
+          bedrooms: 9,
+          description: "Very nice apartment",
+          dailyRate: 33,
+          shared: true,
+          createdAt: "24/12/2017"
+      }]
+      }
+    });
+
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className='App'>
+            <Header/>
+            <div className='container'>
+              <Route exact path = '/'  render = { () => { return <Redirect to='rentals' />} } />
+              <Route exact path = '/rentals' component={RentalList} />
+              <Route exact path = '/rentals/:id' component={RentalDetail} />
+            </div>
+
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,71 +1,12 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
 import { RentalCard } from './RentalCard';
 
-export class RentalList extends React.Component{
-    
-    constructor(){ 
-        super();
-
-        this.state = {
-            rentals: [{
-                id: 1,
-                title: "Central Apartment",
-                city: "New York",
-                street: "Times Sqaure",
-                category: "apartment",
-                image: "http://via.placeholder.com/350x250",
-                bedrooms: 3,
-                description: "Very nice apartment",
-                dailyRate: 34,
-                shared: false,
-                createdAt: "24/12/2017"
-              },
-              {
-                id: 2,
-                title: "Central Apartment 2",
-                city: "San Francisco",
-                street: "Main street",
-                category: "condo",
-                image: "http://via.placeholder.com/350x250",
-                bedrooms: 2,
-                description: "Very nice apartment",
-                dailyRate: 12,
-                shared: true,
-                createdAt: "24/12/2017"
-              },
-              {
-                id: 3,
-                title: "Central Apartment 3",
-                city: "Bratislava",
-                street: "Hlavna",
-                category: "condo",
-                image: "http://via.placeholder.com/350x250",
-                bedrooms: 2,
-                description: "Very nice apartment",
-                dailyRate: 334,
-                shared: true,
-                createdAt: "24/12/2017"
-              },
-              {
-                id: 4,
-                title: "Central Apartment 4",
-                city: "Berlin",
-                street: "Haupt strasse",
-                category: "house",
-                image: "http://via.placeholder.com/350x250",
-                bedrooms: 9,
-                description: "Very nice apartment",
-                dailyRate: 33,
-                shared: true,
-                createdAt: "24/12/2017"
-            }]
-        }
-
-    }
+class RentalList extends React.Component{
 
     renderRentals(){
-        return this.state.rentals.map((rental, index) => {
+        return this.props.rentals.map((rental, index) => {
             return (
                 <RentalCard key={index} 
                             colNum='col-md-3 col-xs-6'
@@ -75,6 +16,10 @@ export class RentalList extends React.Component{
     }
 
     render (){
+
+        debugger;
+        this.props;
+
         return(
             <section id='rentalListing'>
                 <h1 className='page-title'>Your Home All Around the World</h1>
@@ -85,3 +30,14 @@ export class RentalList extends React.Component{
         )
     }
 }
+
+function mapStateToProps(state){
+    
+    debugger;
+
+    return {
+        rentals: state.rentals
+    }
+}
+
+export default connect(mapStateToProps)(RentalList)
