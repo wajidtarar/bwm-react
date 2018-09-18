1f75892941ff76a10497425e184acac55f716c84
Wajid <wajid.tarar@gmail.com>
2018-09-18 12:29:09 +0500

    183
    183 Routing Simple Example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+
 import { Header } from './shared/Header';
 import { RentalList } from './components/rental/RentalList';
+import { RentalDetail } from './components/rental/RentalDetail';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      isRentalList: true
+    }
+
+    this.navigate = this.navigate.bind(this);
+  }
+
+  navigate(){
+    this.setState({
+      isRentalList: !this.state.isRentalList
+    });
+  }
   render() {
     return (
       <div className="App">
         <Header/>
+        <button onClick={this.navigate}>Navigate away</button>
         <div className='container'>
-          <RentalList />
+          { this.state.isRentalList ? <RentalList /> : <RentalDetail /> }
         </div>
 
       </div>
---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export class RentalDetail extends React.Component
+{
+    render(){
+        return(
+            <div>
+                <h1>I am rental detail</h1>
+                <h1>I am rental detail</h1>
+                <h1>I am rental detail</h1>
+                <h1>I am rental detail</h1>
+                <h1>I am rental detail</h1>
+            </div>
+        )
+    }
+}
