10f8b08f94a29f6b67aa462b14d98514c8963e8b
Wajid <wajid.tarar@gmail.com>
2018-09-22 15:42:04 +0500

    207 Separate Routing

--- server/index.js
+++ server/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const config = require('./config/dev');
-const Rental = require('./models/Rental');
+const FakeDb = require('./fake-db');
+const rentalRoutes = require('./routes/rentals');
 
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true });
+mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(()=> {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+});
 
 const app = express();
-
-app.get('/rentals', function(req, res){
-    res.json({'succss': true});
-});
+app.use('/api/v1/rentals/', rentalRoutes);   
 
 const PORT = process.env.PORT || 3001;
 
