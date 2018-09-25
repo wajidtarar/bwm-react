6d37a19e66c2c816da522b0b7f8f1b0c8fbab320
Wajid <wajid.tarar@gmail.com>
2018-09-25 13:00:15 +0500

    219 Google Map Api Key
    219 Google Map Api Key

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from 'actions';
 import {RentalDetailInfo} from './RentalDetailInfo';
-// import {MapWithAMarker} from 'components/map/GoogleMap'
+import {MapWithAMarker} from 'components/map/GoogleMap'
 
 class RentalDetail extends React.Component
 {
@@ -29,6 +29,12 @@ class RentalDetail extends React.Component
                         <img src={rental.image} alt=''></img>
                     </div>
                     <div className='col-md-6'>
+                    <MapWithAMarker
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCa408fUcd0e8KpX_32-Cn1CuWg-vateVc&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: `360px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
                     </div>
                     </div>
                 </div>
