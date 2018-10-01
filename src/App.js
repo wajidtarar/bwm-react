3a958ee9dc026fadca85aa234cfbc1ddb32ec9ec
Wajid <wajid.tarar@gmail.com>
2018-10-01 15:18:42 +0500

    244 Register Redux Form

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter, Route, Redirect} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { Header } from 'shared/Header';
+import { init } from 'reducers';
+
 import RentalListing  from 'components/rental/rental-listing/RentalListing';
 import RentalDetail  from 'components/rental/rental-detail/RentalDetail';
-import { init } from 'reducers';
+import {Login}  from 'components/login/Login';
+import {Register}  from 'components/register/Register';
+
 
 import './App.css';
 
@@ -24,6 +28,8 @@ class App extends Component {
               <Route exact path = '/'  render = { () => { return <Redirect to='rentals' />} } />
               <Route exact path = '/rentals' component={RentalListing} />
               <Route exact path = '/rentals/:id' component={RentalDetail} />
+              <Route exact path = '/login/' component={Login} />
+              <Route exact path = '/register/' component={Register} />
             </div>
 
           </div>
---
+++ src/components/login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class Login extends React.Component{
+    render(){
+        return(
+            <h1>I am login this component DidCatch</h1>
+        );
+    }
+
+}
